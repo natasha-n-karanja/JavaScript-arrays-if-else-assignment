6b0292159dbd9928345d6e827b2f497b2197cd70
natashankaranja <natashankaranja@gmail.com>
2025-06-12 19:33:02 +0300

    positive or negative number

---
+++ if-else-exersise.js
@@ -0,0 +1,10 @@
+//1. Check if a number is positive or negative
+let number = 5;
+
+if (number > 0) {
+  console.log("The number is positive.");
+} else if (number < 0) {
+  console.log("The number is negative.");
+} else {
+  console.log("The number is zero.");
+}
