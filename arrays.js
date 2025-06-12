c42c64299a5032986e7ddbf436cc074d357dd245
natashankaranja <natashankaranja@gmail.com>
2025-06-12 19:13:41 +0300

    added new fruit

--- arrays.js
+++ arrays.js
@@ -1,3 +1,8 @@
 //Create an array of 5 fruits and print the second one
 const fruits = ["apple", "banana", "cherry", "mango", "orange"];
 console.log(fruits[1]); // 'banana' — because arrays are 0-indexed
+
+//Add a new fruit to the end of the array
+fruits.push("grape");
+console.log(fruits);
+
