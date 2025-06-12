2f4f6fb9f9d54ed8ae84a6780a5179b667e9f3c1
natashankaranja <natashankaranja@gmail.com>
2025-06-12 19:16:09 +0300

    storted array

--- arrays.js
+++ arrays.js
@@ -1,11 +1,15 @@
-//Create an array of 5 fruits and print the second one
+//1.Create an array of 5 fruits and print the second one
 const fruits = ["apple", "banana", "cherry", "mango", "orange"];
 console.log(fruits[1]); // 'banana' — because arrays are 0-indexed
 
-//Add a new fruit to the end of the array
+//2.Add a new fruit to the end of the array
 fruits.push("grape");
 console.log(fruits);
 
-//Remove the first element of the array
+//3.Remove the first element of the array
 fruits.shift(); // removes "apple"
+console.log(fruits);
+
+//4. Sort the array in alphabetical order
+fruits.sort();
 console.log(fruits);
